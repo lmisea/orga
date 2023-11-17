e647c04f98280db9c5809ced7a7176247de4f322
Luis Isea <19-10175@usb.ve>
2023-11-17 16:46:33 -0400

    El 1er proyecto lee las teclas wasd

--- proyecto1/agenda.asm
+++ proyecto1/agenda.asm
@@ -11,9 +11,44 @@
 .include "horario.asm"
 
 .data
+key:  	.space 2
+	.ascii "\0"
+	.align 0
+w:	.asciiz "w"
+s:	.asciiz "s"
+a:	.asciiz "a"
+d:	.asciiz "d"
 
 .text
-
-main: 	print_str ("Hola Mundo\n")
-	print_int (10)
+main:	jal leerInput
 	done
+
+leerInput: 	li  $v0, 8
+		la  $a0, key
+		li  $a1, 2
+		syscall
+		print_str ("\n")
+		lb  $t0, key
+		lb  $t1, w
+		beq $t0, $t1, wKey
+		lb  $t1, s
+		beq $t0, $t1, sKey
+		lb  $t1, a
+		beq $t0, $t1, aKey
+		lb  $t1, d
+		beq $t0, $t1, dKey
+		print_str ("Tecla inválida")
+returnToMain:	jr  $ra
+		
+wKey:	print_str ("Se presionó w")
+	j returnToMain
+
+sKey:	print_str ("Se presionó s")
+	j returnToMain
+
+aKey:	print_str ("Se presionó a")
+	j returnToMain
+
+dKey:	print_str ("Se presionó d")
+	j returnToMain
+
