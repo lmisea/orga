855632fd76eb9fbaed73a80102bc0cd3b5e3f741
Luis Isea <19-10175@usb.ve>
2023-11-19 20:50:09 -0400

    primer boceto de agregarCursoAlHorario()

--- proyecto1/agenda.asm
+++ proyecto1/agenda.asm
@@ -11,7 +11,34 @@
 .include "horario.asm"
 
 .data
+newline: 	.asciiz "\n"
+space_char:	.asciiz " "
+horizontalLine: .asciiz "---------------------------------------\n"
+separator: 	.asciiz "|"
+up:		.asciiz "|^"
+down:		.asciiz "|v"
+left_and_right: .asciiz "<-A | D->"
+hyphen: 	.asciiz "–"
+
+T:		.asciiz "T"
+L:		.asciiz "L"
+
+lun: 		.space 16 	# Cada día tiene 8 bloques de clases posibles (01,02,...,08)
+				# Y cada bloque tiene un carácter <T,L> y el núm del curso del
+				# cual es la clase
+mar: 		.space 16
+mie: 		.space 16
+jue:		.space 16
+vie:		.space 16
+sab:		.space 16
 
 .text
-main:	readKey()
+main:	leerCOD(C1N)
+	leerEDF(C1N)
+	leerNOM(C1N)
+	print_str ("\n")
+	agregarCursoAlHorario(C1N, C1H, 1, lun, mar, mie, jue, vie, sab) 
+	agregarCursoAlHorario(C2N, C2H, 2, lun, mar, mie, jue, vie, sab)
+	print_str ("\n")
+	readKey()
 	done
--- proyecto1/macros.asm
+++ proyecto1/macros.asm
@@ -22,24 +22,27 @@
 # Ejemplo de uso: print_str ("Hola Mundo")
 .macro print_str (%str)
 	.data
-myLabel:.asciiz %str
+text:	.asciiz %str
 	.text
 	li $v0, 4
-	la $a0, myLabel
+	la $a0, text
 	syscall
 .end_macro
 
-# Macro para imprimir un space
-# Macro para imprimir un espacio reservado
-.macro print_space(%buffer, %length)
+# Macro para imprimir un space de ascii
+# %buffer: Label que se quiere imprimir
+# %start: A partir de qué byte de %buffer se va a imprimir
+# %length: Cuántos carácteres se quieren imprimir
+.macro print_space(%buffer, %start, %length)
     .data
-    buffer_str: .space 40  # Ajustar el tama�o seg�n tus necesidades
+    buffer_str: .space %length  # Ajustar el tamaño según tus necesidades
     .text
 
     # Copiar el contenido del buffer al espacio reservado
     la $t0, %buffer
     la $t1, buffer_str
     li $t2, %length
+    add $t0, $t0, %start  # Mover al inicio especificado
     loop_copy:
         lb $t3, 0($t0)
         sb $t3, 0($t1)
@@ -51,10 +54,6 @@ myLabel:.asciiz %str
     # Imprimir el contenido del espacio reservado
     li $v0, 4
     la $a0, buffer_str
-    syscall
-
-    li $v0, 4
-    la $a0, newline  # Asumiendo que hay una cadena de nueva l�nea definida
     syscall
 .end_macro
 
@@ -143,42 +142,114 @@ myLabel:.asciiz %str
 .end_macro
 
 
-# LEER CiN
-# Macro para leer CiN
-# %dest: Registro o direcci�n donde se almacenar� la cadena
-# %src: Direcci�n de la cadena a copiar
-# %start: Posici�n de inicio en la cadena (bit o byte)
+# LEER Datos de un CN
+
+# Macro para leer un atributo de un CiN
+# %dest: Registro o dirección donde se almacenará la cadena
+# %src: Direccián de la cadena a copiar
+# %start: Posición de inicio en la cadena
 # %length: Longitud de la cadena a copiar
-.macro leerC(%dest, %src, %start, %length)
+.macro leerC(%dest, %destStart, %src, %srcStart, %length)
     .data
     buffer: .space %length
     .text
 
     # Copiar el contenido
-    la $t0, %src
-    la $t1, buffer
-    li $t2, %length
-    add $t0, $t0, %start  # Mover al inicio especificado
+    la  $t0, %src
+    la  $t1, buffer
+    li  $t2, %length
+    add $t0, $t0, %srcStart  # Mover al inicio especificado
     loop_copy:
-        lb $t3, 0($t0)
-        sb $t3, 0($t1)
+        lb   $t3, 0($t0)
+        sb   $t3, 0($t1)
         addi $t0, $t0, 1
         addi $t1, $t1, 1
         subi $t2, $t2, 1
         bnez $t2, loop_copy
 
-    # Imprimir el contenido
-    print_space (buffer, %length)
-
     # Copiar el contenido al destino
-    la $t0, %dest
-    la $t1, buffer
-    li $t2, %length
+    la  $t0, %dest
+    la  $t1, buffer
+    li  $t2, %length
+    add $t0, $t0, %destStart  # Mover al inicio especificado
     loop_copy_dest:
-        lb $t3, 0($t1)
-        sb $t3, 0($t0)
+        lb   $t3, 0($t1)
+        sb   $t3, 0($t0)
         addi $t0, $t0, 1
         addi $t1, $t1, 1
         subi $t2, $t2, 1
         bnez $t2, loop_copy_dest
+.end_macro
+
+# Macro para leer el COD-### de un CN
+.macro leerCOD(%CN)
+	print_space (%CN, 0, 8)
+.end_macro 
+
+# Macro para leer el EDF-### de un CN
+.macro leerEDF(%CN)
+	print_space (%CN, 8, 8)
+.end_macro
+
+# Macro para leer el NOMBRECURSO de un CN
+.macro leerNOM(%CN)
+	print_space (%CN, 16, 40)
+.end_macro
+
+.macro agregarCursoAlHorario(%CN, %CH, %numCurso, %lun, %mar, %mie, %jue, %vie, %sab)
+	.data
+	T:	.asciiz "T"
+	L:	.asciiz "L"
+	.text
+	la  $t0, %CH
+    	li  $t2, 0
+    	revisarDia:
+    		   bnez $t2, siguienteDia
+    	verificar: lb   $t3, 0($t0)
+        	   lb   $t4, T
+        	   beq  $t3, $t4, teoria
+        	   lb   $t4, L
+        	   beq  $t3, $t4, lab
+        continue:  add  $t2, $t2, 1
+       		   bne  $t2, 6, revisarDia
+        	   j end
+        
+        teoria: # Agregamos en el día $t2 la clase T%numCurso
+        	print_str ("T el ")
+        	print_int ($t2)
+        	print_str (", desde 0")
+        	add $t0, $t0, 2  	# Obtenemos la hora inicial
+        	lb  $t3, 0($t0)
+        	andi $t3, $t3,0x0F 	# convertimos $t3 de ascii a un int
+        	print_int ($t3)
+        	print_str (" hasta 0")
+        	add $t0, $t0, 2  	# Obtenemos la hora final
+        	lb  $t3, 0($t0)
+        	andi $t3, $t3,0x0F 	# convertimos $t3 de ascii a un int
+        	print_int ($t3)
+        	print_str ("\n")
+        	sub $t0, $t0, 4 
+        	j continue
+        
+        lab: 	# Agregamos en el día $t2 la clase L%numCurso
+        	print_str ("L el ")
+        	print_int ($t2)
+        	print_str (", desde 0")
+        	add $t0, $t0, 2  	# Obtenemos la hora inicial
+        	lb  $t3, 0($t0)
+        	andi $t3, $t3,0x0F 	# convertimos $t3 de ascii a un int
+        	print_int ($t3)
+        	print_str (" hasta 0")
+        	add $t0, $t0, 2  	# Obtenemos la hora final
+        	lb  $t3, 0($t0)
+        	andi $t3, $t3,0x0F 	# convertimos $t3 de ascii a un int
+        	print_int ($t3)
+        	print_str ("\n")
+        	sub $t0, $t0, 4 
+        	j continue
+        
+        siguienteDia: 	add $t0, $t0, 5
+        		j verificar
+        
+        end:
 .end_macro
